fefbb675ccaee2034a44e6d1f440a0a46fa8cccd
karen-elisha <karenelisha0204@gmail.com>
2026-07-14 21:55:50 +0530

    36.ValidSuduko

---
+++ karen/36.ValidSuduko/36.validSuduko.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+
+        HashSet<Character>[] rows = new HashSet[9];
+        HashSet<Character>[] cols = new HashSet[9];
+        HashSet<Character>[] boxes = new HashSet[9];
+
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            boxes[i] = new HashSet<>();
+        }
+
+        for (int i = 0; i < 9; i++) {
+
+            for (int j = 0; j < 9; j++) {
+
+                char num = board[i][j];
+
+                if (num == '.')
+                    continue;
+
+                int boxIndex = (i / 3) * 3 + (j / 3);
+
+                if (rows[i].contains(num))
+                    return false;
+
+                if (cols[j].contains(num))
+                    return false;
+
+                if (boxes[boxIndex].contains(num))
+                    return false;
+
+                rows[i].add(num);
+                cols[j].add(num);
+                boxes[boxIndex].add(num);
+            }
+        }
+
+        return true;
+    }
+}
